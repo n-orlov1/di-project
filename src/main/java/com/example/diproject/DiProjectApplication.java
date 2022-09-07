0cfa7bcec2f15709c0541cc88dbeb19bea7a4e64
Nikita Orlov <norlov@yasp.ru>
2022-09-07 22:29:02 +0300

    Project init

---
+++ src/main/java/com/example/diproject/DiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.diproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diproject/DiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
